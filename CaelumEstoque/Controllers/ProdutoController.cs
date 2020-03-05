70a10810969e2fe8e3979781bdc79e8c6d5f96ac
lucas.soares <lucas.soares@NGS-321>
2020-03-05 15:29:58 -0300

    Add project files.

---
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.Models;
+using CaelumEstoque.DAO;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Index()
+        {
+
+            ProdutosDAO dao = new ProdutosDAO();
+
+            IList<Produto> produtos = dao.Lista();
+
+            ViewBag.Produtos = produtos;
+
+
+
+            return View();
+        }
+    }
+}
