be75e8b927fcd284483ae2a74ba5dccd7a7514ef
lucas.soares <lucas.soares@NGS-321>
2020-03-05 17:21:51 -0300

    inicia construção do metodo Acidiona usando entity framework para interação com database.

--- CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -15,14 +15,35 @@ namespace CaelumEstoque.Controllers
         {
 
             ProdutosDAO dao = new ProdutosDAO();
-
             IList<Produto> produtos = dao.Lista();
-
             ViewBag.Produtos = produtos;
-
-
 
             return View();
         }
+
+        public ActionResult Form()
+        {
+
+            return View();
+        }
+
+        public ActionResult Adiciona(String nome, float preco, int quantidade, String descricao, int categoriaId)
+        {
+            Produto produto = new Produto() {
+                Nome = nome,
+                Preco = preco,
+                Quantidade = quantidade,
+                Descricao = descricao,
+                CategoriaId = categoriaId
+            };
+
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+
+            return View();
+
     }
+
+
+}
 }
